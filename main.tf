e9268f41fa352749d743270153952b13deb063a6
J-odo <mijulius30@gmail.com>
2023-02-26 23:35:15 -0600

    updated terraform code for tf cloud

--- main.tf
+++ main.tf
@@ -1,13 +1,22 @@
 provider "aws" {
-  region  = "us-east-1"
-  profile = "production"
+  region  = var.AWS_REGION
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
+variable "AWS_ACCESS_KEY_ID" {}
+variable "AWS_SECRET_ACCESS_KEY" {}
+variable "AWS_REGION" {
+}
+
+
 terraform {
-  backend "s3" {
-    bucket = "mystate-2023"
-    key    = "myec2state"
-    region = "us-east-1"
+  cloud {
+    organization = "visual-greatness-class"
+
+    workspaces {
+      name = "awsec2-deploy"
+    }
   }
 }
 

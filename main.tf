1c8e62fcede7112afdcf9f311c1d3762b53ad767
J-odo <mijulius30@gmail.com>
2023-02-27 00:02:30 -0600

    renamed web servers

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "aws_instance" "test-1" {
   ami           = "ami-0dfcb1ef8550277af"
   instance_type = "t2.micro"
   tags = {
-    "Name" = "mysecondec2"
+    "Name" = "randomec2"
   }
 }
 
@@ -41,6 +41,6 @@ resource "aws_instance" "test-3" {
   ami           = "ami-0dfcb1ef8550277af"
   instance_type = "t2.micro"
   tags = {
-    "Name" = "mythirdec2"
+    "Name" = "automationec2"
   }
 }
